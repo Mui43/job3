7506acb517b77bdaa6b1969a5c72a3a7313ff719
tanapat muijeen <tanapat.muijeen@gmail.com>
2024-06-12 14:08:27 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private navCtrl: NavController) { }
+
+  goabout() {
+    this.navCtrl.navigateForward('/tabs/tab2');
+    }
+    gocontact() {
+      this.navCtrl.navigateForward('/tabs/tab3');
+      }
 
 }
